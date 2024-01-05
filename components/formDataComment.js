304b838b3ae2e6d169faf1c4c7c707d0900bd6c1
Elizaveta810 <elizaveta.k810@mail.ru>
2024-01-06 00:19:53 +0300

    Произведена авторизация

---
+++ components/formDataComment.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+export const formDataComment = (commentDate) => {
+  const dateComment = new Date(commentDate);
+ const formatDate =
+   dateComment.getDate().toString().padStart(2, '0') + '.' +
+   (dateComment.getMonth() + 1).toString().padStart(2, '0') + '.' +
+   dateComment.getFullYear().toString().slice(-2) + ' ' +
+   dateComment.getHours().toString().padStart(2, '0') + ':' +
+   dateComment.getMinutes().toString().padStart(2, '0');
+   return formatDate
+}
--- components/render.js
+++ components/render.js
@@ -1,42 +1,18 @@
-import {comments } from "../main.js";
-
-const loadingCommentEl = document.getElementById("loadingComment");
-const loadingTextEl = document.getElementById("loadingText");  
-const listElement = document.getElementById("comments");
-const form = document.getElementById("add-form");
+import {comments, fetchAndRenderComments, user} from "../main.js";
+import { renderLogin} from "./renderLogin.js";
+import { postComment, token } from "./api.js";
 
 
-//Счетчик лайков
-const initLikeListeners = () => {
-  const likeButtons = document.querySelectorAll(".like-button");
-  for (const likeButton of likeButtons) {
-    likeButton.addEventListener("click", (event) => {
-      event.stopPropagation();
-      const index = likeButton.dataset.index;
-      comments[index].likes += comments[index].isLiked ? -1 : +1;
-      comments[index].isLiked = !comments[index].isLiked;
+const sanitizeHtml = (htmlString) => {
+  return htmlString.replaceAll("<", "&lt;").replaceAll(">", "&gt;");};
 
-      renderComment();
-    });
-  }
-};
 
-//Цитата
-const initCommentListeners = () => {
-  const textInputElement = document.getElementById("add-form-text");
-  const commentButtons = document.querySelectorAll(".comment");
-  for (const commentButton of commentButtons) {
-    commentButton.addEventListener("click", () => {
-      const index = commentButton.dataset.index;
-      textInputElement.value = `${comments[index].name}:\n ${comments[index].text}`;
-    });
-  }
-};
+
 
 
 export const renderComment = () => {
- 
-
+  const appElement = document.getElementById("app");
+//Добавление Разметки списка комментариев(лента всех комментариев)
   const commentHtml = comments
     .map((comment, index) => {
       return `<li class="comment" data-index="${index}">
@@ -61,17 +37,157 @@ export const renderComment = () => {
     })
     .join("");
 
-  loadingTextEl.style.display = "none"; // При загрузке ленты комментария, сообщение убирается
-  loadingCommentEl.style.display = "none";
-  form.style.display = "flex";
+    // Форма добавления комментария
+    const formHtml = () => {
+      if (!token) return btnLogin;
+      return `
+    <div id="add-form" class="add-form">
+    <input type="text" id="name-input" class="add-form-name" value="${user.name}" readonly/> 
+    <textarea type="textarea" id="textarea-input" class="add-form-text" placeholder="Введите ваш коментарий"
+      rows="4"></textarea>
+    <div class="add-form-row">
+      <button id="add-button" class="add-form-button">Написать</button>
+    </div>
+  
+  </div>
+  <div id="add-comment" ></div>
+  `
+    }
+  
+    const btnLogin = `
+  <p class="render-login-btn">  Чтобы добавить комментарий, <u>авторизуйтесь</u> </p>
+  `
+  //Активация кнопки АВТОРИЗИРУЙТЕСЬ
+    function actionRenderLoginbtn() {
+      if (token) return
+      const btn = document.querySelector(".render-login-btn")
+      btn.addEventListener('click', () => {
+        renderLogin()
+      })
+    }
+  
+    const appHtml = `
+      <div class="container">
+      <ul id="list" class="comments">
+  
+  ${commentHtml}
+      </ul>
+     ${formHtml()}
+    </div>
+      `;
+  
+    appElement.innerHTML = appHtml;
+  
+    actionRenderLoginbtn();
 
-  listElement.innerHTML = commentHtml;
- // textInputElement.value = "";
- // nameInputElement.value = "";
 
-  initLikeListeners();
-  initCommentListeners();
+
+
+// new comment
+
+const addNewComment = () => {
+
+  const textareaInputElement = document.getElementById("textarea-input");
+
+
+  textareaInputElement.classList.remove("error");
+
+  if (textareaInputElement.value === "") {
+    textareaInputElement.classList.add("error");
+
+    return;
+  }
+
+
+  const addForm = document.getElementById("add-form");
+  const addComment = document.getElementById("add-comment");
+
+  addForm.classList.add("hidden");
+
+  addComment.innerHTML = "Элемент добавляется...";
+  console.log(addComment.innerHTML);
+
+
+
+  postComment({
+    text: sanitizeHtml(textareaInputElement.value),
+
+  })
+    .then((response) => {
+      if (response.status === 201) {
+
+        fetchAndRenderComments()
+
+
+
+        addForm.classList.remove("hidden");
+        addComment.classList.add("hidden");
+        textareaInputElement.value = '';
+
+        return
+      }
+      if (response.status === 400) {
+        return Promise.reject("вы ввели имя короче 3-х символов");
+      }
+      if (response.status === 500) {
+        return Promise.reject("ошибка сервера");
+      }
+      return Promise.reject("сервер упал");
+
+    })
+    .catch((error) => {
+      addForm.classList.remove("hidden");
+      addComment.classList.add("hidden");
+      alert(error);
+      //todo:отправлять в систему сбора ошибок??
+      console.warn(error);
+    })
+
+};
+
+if (token) {
+  const buttonElement = document.getElementById('add-button');
+
+  buttonElement.addEventListener('click', addNewComment);
+}
+
+initLikeListeners();
+initCommentListeners();
+
 };
 
 
- 
+
+
+
+
+ //Счетчик лайков
+const initLikeListeners = () => {
+  const likeButtons = document.querySelectorAll(".like-button");
+  for (const likeButton of likeButtons) {
+    likeButton.addEventListener("click", (event) => {
+      event.stopPropagation();
+      if (!token) {
+        alert ('Авторизуйтесь')
+        return
+      }
+      const index = likeButton.dataset.index;
+      comments[index].likes += comments[index].isLiked ? -1 : +1;
+      comments[index].isLiked = !comments[index].isLiked;
+
+      renderComment();
+    });
+  }
+};
+
+//Цитата
+const initCommentListeners = () => {
+  const textareaInputElement = document.getElementById("textarea-input");
+  const commentButtons = document.querySelectorAll(".comment");
+  for (const commentButton of commentButtons) {
+    commentButton.addEventListener("click", () => {
+      const index = commentButton.dataset.index;
+      textareaInputElement.value = `${comments[index].name}:\n ${comments[index].text}`;
+    });
+  }
+};
